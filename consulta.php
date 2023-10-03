37774ff2d740a35fe73d88d28ead89906a92d09f
PenguinGuerrilla <uni.hsilva@gmail.com>
2023-10-02 23:17:43 -0300

    api de cep adicionada

---
+++ consulta.php
@@ -0,0 +1,21 @@
+<?php
+
+$cep = $_POST['CEP'];
+
+$retorno = json_decode(file_get_contents("https://viacep.com.br/ws/$cep/json/"));
+
+//var_dump($retorno);
+
+print("<h1>Resultado da consulta</h1>");
+print("Logradouro: ".$retorno->logradouro."<br>");
+
+if($retorno->complemento <> "") {
+   print("Complemento: ".$retorno->complemento."<br>");
+}
+
+
+print("Bairro: ".$retorno->bairro."<br>");
+print("Cidade: ".$retorno->localidade."<br>");
+print("Estado: ".$retorno->uf);
+
+?>
